efa07e3d73d6daf4c1ccef75f6c0132fb0476bb0
Paul PASSERON <paul.passeron@ensiie.eu>
2025-10-22 18:19:22 +0200

    Add variable support to arithmetic expressions and evaluation
    Introduce a variable constructor to ArExpr and extend evaluation,
    small-step semantics, and proofs to handle variables with an
    environment.

--- ExprEval/Basic.lean
+++ ExprEval/Basic.lean
@@ -1,44 +1,60 @@
+variable (V: Type)
+
 inductive ArExpr: Type
     | Const: Int -> ArExpr
     | Add: ArExpr -> ArExpr -> ArExpr
     | Sub: ArExpr -> ArExpr -> ArExpr
     | Mul: ArExpr -> ArExpr -> ArExpr
+    | Var: V -> ArExpr
 
-def eval (e: ArExpr): Int :=
+
+def eval (val: V -> Int) (e: ArExpr V) : Int :=
     match e with
         | ArExpr.Const x => x
-        | ArExpr.Add lhs rhs => (eval lhs) + (eval rhs)
-        | ArExpr.Sub lhs rhs => (eval lhs) - (eval rhs)
-        | ArExpr.Mul lhs rhs => (eval lhs) * (eval rhs)
-
+        | ArExpr.Add lhs rhs => (eval val lhs) + (eval val rhs)
+        | ArExpr.Sub lhs rhs => (eval val lhs) - (eval val rhs)
+        | ArExpr.Mul lhs rhs => (eval val lhs) * (eval val rhs)
+        | ArExpr.Var v => val v
 
 -- My small steps semantic
 
-inductive Step: ArExpr -> ArExpr -> Prop where
+inductive Step: (val: V -> Int) -> (ArExpr V) -> (ArExpr V) -> Prop where
+    | getVarValue (var: V) :
+        Step
+            val
+            (ArExpr.Var var)
+            (ArExpr.Const (val var))
     | addConstConst(n1 n2: Int) :
         Step
+            val
             (ArExpr.Add (ArExpr.Const n1) (ArExpr.Const n2))
             (ArExpr.Const (n1 + n2))
     | subConstConst(n1 n2: Int) :
         Step
+            val
             (ArExpr.Sub (ArExpr.Const n1) (ArExpr.Const n2))
             (ArExpr.Const (n1 - n2))
     | mulConstConst(n1 n2: Int) :
         Step
+            val
             (ArExpr.Mul (ArExpr.Const n1) (ArExpr.Const n2))
             (ArExpr.Const (n1 * n2))
-    | addLeft (e1 e1' e2) : Step e1 e1' -> Step (ArExpr.Add e1 e2) (ArExpr.Add e1' e2)
-    | subLeft (e1 e1' e2) : Step e1 e1' -> Step (ArExpr.Sub e1 e2) (ArExpr.Sub e1' e2)
-    | mulLeft (e1 e1' e2) : Step e1 e1' -> Step (ArExpr.Mul e1 e2) (ArExpr.Mul e1' e2)
-    | addRight (n: Int) (e2 e2': ArExpr) : Step e2 e2' -> Step (ArExpr.Add (ArExpr.Const n) e2)  (ArExpr.Add (ArExpr.Const n) e2')
-    | subRight (n: Int) (e2 e2': ArExpr) : Step e2 e2' -> Step (ArExpr.Sub (ArExpr.Const n) e2)  (ArExpr.Sub (ArExpr.Const n) e2')
-    | mulRight (n: Int) (e2 e2': ArExpr) : Step e2 e2' -> Step (ArExpr.Mul (ArExpr.Const n) e2)  (ArExpr.Mul (ArExpr.Const n) e2')
+    | addLeft (e1 e1' e2) : Step val e1 e1' -> Step val (ArExpr.Add e1 e2) (ArExpr.Add e1' e2)
 
-inductive StepStar : ArExpr → ArExpr → Prop where
-    | refl e : StepStar e e
-    | trans e₁ e₂ e₃ : Step e₁ e₂ → StepStar e₂ e₃ → StepStar e₁ e₃
+    | subLeft (e1 e1' e2) : Step val e1 e1' -> Step val (ArExpr.Sub e1 e2) (ArExpr.Sub e1' e2)
 
-theorem step_preserve_eval (e e': ArExpr): Step e e' -> eval e = eval e' := by
+    | mulLeft (e1 e1' e2) : Step val e1 e1' -> Step val (ArExpr.Mul e1 e2) (ArExpr.Mul e1' e2)
+
+    | addRight (n: Int) (e2 e2': ArExpr V) : Step val e2 e2' -> Step val (ArExpr.Add (ArExpr.Const n) e2)  (ArExpr.Add (ArExpr.Const n) e2')
+
+    | subRight (n: Int) (e2 e2': ArExpr V) : Step val e2 e2' -> Step val (ArExpr.Sub (ArExpr.Const n) e2)  (ArExpr.Sub (ArExpr.Const n) e2')
+    | mulRight (n: Int) (e2 e2': ArExpr V) : Step val e2 e2' -> Step val (ArExpr.Mul (ArExpr.Const n) e2)  (ArExpr.Mul (ArExpr.Const n) e2')
+
+inductive StepStar : (val: V -> Int) -> (ArExpr V) -> (ArExpr V) -> Prop where
+    | refl val e : StepStar val e e
+    | trans e₁ e₂ e₃ : (Step V val) e₁ e₂ -> StepStar val e₂ e₃ -> StepStar val e₁ e₃
+
+theorem step_preserve_eval (e e': ArExpr V): (Step V val) e e' -> eval V val e = eval V val e' := by
     intro h
     induction h with
 
@@ -54,17 +70,22 @@ theorem step_preserve_eval (e e': ArExpr): Step e e' -> eval e = eval e' := by
     | subRight n e2 e1' _ ih
     | mulRight n e2 e1' _ ih => simp [eval, ih]
 
-theorem stepstar_preserves_eval (e e': ArExpr) : StepStar e e' → eval e = eval e' := by
+    | getVarValue var => simp [eval]
+
+
+theorem stepstar_preserves_eval (e e': ArExpr V) :
+    StepStar V val e e' -> eval V val e = eval V val e' := by
     intro h
     induction h with
     | refl _ => rfl
-    | trans _ _ _ h1 _ ih => rw [step_preserve_eval _ _ h1, ih]
+    | trans _ _ _ h1 _ ih => rw [step_preserve_eval _ _ _ h1, ih]
 
-theorem soundness (e: ArExpr) (v: Int) : StepStar e (ArExpr.Const v) → eval e = v := by
-    exact stepstar_preserves_eval _ _
+theorem soundness (e: ArExpr V) (v: Int) :
+    StepStar V val e (ArExpr.Const v) -> eval V val e = v := by
+    exact stepstar_preserves_eval _ _ _
 
-theorem stepstar_add_left (e1 e1' e2: ArExpr) :
-    StepStar e1 e1' → StepStar (ArExpr.Add e1 e2) (ArExpr.Add e1' e2) := by
+theorem stepstar_add_left (e1 e1' e2: ArExpr V) :
+    StepStar V val e1 e1' -> StepStar V val (ArExpr.Add e1 e2) (ArExpr.Add e1' e2) := by
     intro h
     induction h with
     | refl _ => apply StepStar.refl
@@ -73,8 +94,11 @@ theorem stepstar_add_left (e1 e1' e2: ArExpr) :
         . exact Step.addLeft _ _ _ step
         . exact ih
 
-theorem stepstar_add_right (n: Int) (e2 e2': ArExpr) :
-    StepStar e2 e2' → StepStar (ArExpr.Add (ArExpr.Const n) e2) (ArExpr.Add (ArExpr.Const n) e2') := by
+theorem stepstar_add_right (n: Int) (e2 e2': ArExpr V) :
+    StepStar V val e2 e2' ->
+        StepStar V val
+            (ArExpr.Add (ArExpr.Const n) e2)
+            (ArExpr.Add (ArExpr.Const n) e2') := by
     intro h
     induction h with
     | refl _ => apply StepStar.refl
@@ -85,8 +109,11 @@ theorem stepstar_add_right (n: Int) (e2 e2': ArExpr) :
 
 
 
-theorem stepstar_sub_left (e1 e1' e2: ArExpr) :
-    StepStar e1 e1' → StepStar (ArExpr.Sub e1 e2) (ArExpr.Sub e1' e2) := by
+theorem stepstar_sub_left (e1 e1' e2: ArExpr V) :
+    StepStar V val e1 e1' ->
+        StepStar V val
+            (ArExpr.Sub e1 e2)
+            (ArExpr.Sub e1' e2) := by
     intro h
     induction h with
     | refl _ => apply StepStar.refl
@@ -95,8 +122,11 @@ theorem stepstar_sub_left (e1 e1' e2: ArExpr) :
         . exact Step.subLeft _ _ _ step
         . exact ih
 
-theorem stepstar_sub_right (n: Int) (e2 e2': ArExpr) :
-    StepStar e2 e2' → StepStar (ArExpr.Sub (ArExpr.Const n) e2) (ArExpr.Sub (ArExpr.Const n) e2') := by
+theorem stepstar_sub_right (n: Int) (e2 e2': ArExpr V) :
+    StepStar V val e2 e2' ->
+        StepStar V val
+            (ArExpr.Sub (ArExpr.Const n) e2)
+            (ArExpr.Sub (ArExpr.Const n) e2') := by
     intro h
     induction h with
     | refl _ => apply StepStar.refl
@@ -107,8 +137,11 @@ theorem stepstar_sub_right (n: Int) (e2 e2': ArExpr) :
 
 
 
-theorem stepstar_mul_left (e1 e1' e2: ArExpr) :
-    StepStar e1 e1' → StepStar (ArExpr.Mul e1 e2) (ArExpr.Mul e1' e2) := by
+theorem stepstar_mul_left (e1 e1' e2: ArExpr V) :
+    StepStar V val e1 e1' ->
+        StepStar V val
+            (ArExpr.Mul e1 e2)
+            (ArExpr.Mul e1' e2) := by
     intro h
     induction h with
     | refl _ => apply StepStar.refl
@@ -117,8 +150,11 @@ theorem stepstar_mul_left (e1 e1' e2: ArExpr) :
         . exact Step.mulLeft _ _ _ step
         . exact ih
 
-theorem stepstar_mul_right (n: Int) (e2 e2': ArExpr) :
-    StepStar e2 e2' → StepStar (ArExpr.Mul (ArExpr.Const n) e2) (ArExpr.Mul (ArExpr.Const n) e2') := by
+theorem stepstar_mul_right (n: Int) (e2 e2': ArExpr V) :
+    StepStar V val e2 e2' ->
+        StepStar V val
+            (ArExpr.Mul (ArExpr.Const n) e2)
+            (ArExpr.Mul (ArExpr.Const n) e2') := by
     intro h
     induction h with
     | refl _ => apply StepStar.refl
@@ -127,7 +163,10 @@ theorem stepstar_mul_right (n: Int) (e2 e2': ArExpr) :
         . apply Step.mulRight _ _ _ step1
         . exact ih
 
-theorem chasle_step_star {e1 e2 e3: ArExpr}: StepStar e1 e2 -> StepStar e2 e3 -> StepStar e1 e3 := by
+theorem chasle_step_star {e1 e2 e3: ArExpr V}:
+    StepStar V val e1 e2 ->
+        StepStar V val e2 e3 ->
+            StepStar V val e1 e3 := by
     intros h1 h2
     induction h1 with
     | refl e => exact h2
@@ -136,38 +175,43 @@ theorem chasle_step_star {e1 e2 e3: ArExpr}: StepStar e1 e2 -> StepStar e2 e3 ->
         have h4 := StepStar.trans _ _ _ step h3
         exact h4
 
-theorem completeness (e: ArExpr) : StepStar e (ArExpr.Const (eval e)) := by
+theorem completeness (e: ArExpr V) : StepStar V val e (ArExpr.Const (eval V val e)) := by
     induction e with
     | Const x =>
         simp [eval]
-        exact StepStar.refl _
+        exact StepStar.refl _ _
     | Add e1 e2 ih1 ih2 =>
         -- ih1 : StepStar e1 (ArExpr.Const (eval e1))
         -- ih2 : StepStar e2 (ArExpr.Const (eval e2))
         simp [eval]
-        have a := stepstar_add_left _ _ e2 ih1
-        have b := stepstar_add_right (eval e1) e2 _ ih2
-        have c := Step.addConstConst (eval e1) (eval e2)
-        have d := StepStar.trans _ _ _ c (StepStar.refl (ArExpr.Const (eval e1 + eval e2)))
-        have e := chasle_step_star a (chasle_step_star b d)
+        have a := stepstar_add_left _ _ _ e2 ih1
+        have b := stepstar_add_right _ (eval V val e1) e2 _ ih2
+        have c := Step.addConstConst (val := val) (eval V val e1) (eval V val e2)
+        have d := StepStar.trans _ _ _ c (StepStar.refl _ (ArExpr.Const (eval V val e1 + eval V val e2)))
+        have e := chasle_step_star _ a (chasle_step_star _ b d)
         exact e
     | Sub e1 e2 ih1 ih2 =>
         -- ih1 : StepStar e1 (ArExpr.Const (eval e1))
         -- ih2 : StepStar e2 (ArExpr.Const (eval e2))
         simp [eval]
-        have a := stepstar_sub_left _ _ e2 ih1
-        have b := stepstar_sub_right (eval e1) e2 _ ih2
-        have c := Step.subConstConst (eval e1) (eval e2)
-        have d := StepStar.trans _ _ _ c (StepStar.refl (ArExpr.Const (eval e1 - eval e2)))
-        have e := chasle_step_star a (chasle_step_star b d)
+        have a := stepstar_sub_left _ _ _ e2 ih1
+        have b := stepstar_sub_right _ (eval V val e1) e2 _ ih2
+        have c := Step.subConstConst (val := val) (eval V val e1) (eval V val e2)
+        have d := StepStar.trans _ _ _ c (StepStar.refl _ (ArExpr.Const  (eval V val e1 - eval V val e2)))
+        have e := chasle_step_star _ a (chasle_step_star _ b d)
         exact e
     | Mul e1 e2 ih1 ih2 =>
         -- ih1 : StepStar e1 (ArExpr.Const (eval e1))
         -- ih2 : StepStar e2 (ArExpr.Const (eval e2))
         simp [eval]
-        have a := stepstar_mul_left _ _ e2 ih1
-        have b := stepstar_mul_right (eval e1) e2 _ ih2
-        have c := Step.mulConstConst (eval e1) (eval e2)
-        have d := StepStar.trans _ _ _ c (StepStar.refl (ArExpr.Const (eval e1 * eval e2)))
-        have e := chasle_step_star a (chasle_step_star b d)
+        have a := stepstar_mul_left _ _ _ e2 ih1
+        have b := stepstar_mul_right _ (eval V val e1) e2 _ ih2
+        have c := Step.mulConstConst (val := val) (eval V val e1) (eval V val e2)
+        have d := StepStar.trans _ _ _ c (StepStar.refl _ (ArExpr.Const (eval  V val e1 * eval V val e2)))
+        have e := chasle_step_star _ a (chasle_step_star _ b d)
         exact e
+    | Var var =>
+        simp [eval]
+        have a := Step.getVarValue var (val := val)
+        have b := StepStar.refl val (ArExpr.Const (val var))
+        exact StepStar.trans (val := val) _ _ _ a b
